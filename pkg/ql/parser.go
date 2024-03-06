320e8989241867b62ea1cfa684d0eebab75542ac
kbabushkin <konstantin.babushkin@acquire.io>
2024-03-06 15:58:05 -0800

    copy paste ql parser for now

---
+++ pkg/ql/parser.go
@@ -0,0 +1,174 @@
+// Copyright 2024 The Solaris Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ql
+
+import (
+	"fmt"
+	"github.com/alecthomas/participle/v2"
+	"github.com/alecthomas/participle/v2/lexer"
+	"strings"
+)
+
+type (
+	// Expression is an AST element which describes a series of OR conditions
+	Expression struct {
+		Or []*OrCondition `@@ { "OR" @@ }`
+	}
+
+	// OrCondition is an AST element which describes a series of AND conditions
+	OrCondition struct {
+		And []*XCondition `@@ { "AND" @@ }`
+	}
+
+	// XCondition is an AST element which groups either a Condition object or an Expression object
+	XCondition struct {
+		Not  bool        ` [@"NOT"] `
+		Cond *Condition  `( @@`
+		Expr *Expression `| "(" @@ ")")`
+	}
+
+	// Condition is a unary or binary logical operation which has first mandatory param and
+	// optional operation and second param
+	Condition struct {
+		FirstParam  Param  `  @@`
+		Op          string ` {@("<"|">"|">="|"<="|"!="|"="|"IN"|"LIKE")`
+		SecondParam *Param ` @@}`
+	}
+
+	// Param describes a parameter either a constant (string or number), function, identifier or an array of constants
+	Param struct {
+		Const      *Const    ` @@`
+		Function   *Function ` | @@`
+		Identifier string    ` | @Ident`
+		Array      []*Const  `|"[" (@@ {"," @@})?"]"`
+	}
+
+	// Const contains the constant either string or float32 value
+	Const struct {
+		Number float32 ` @Number`
+		String string  ` | @String`
+	}
+
+	// Function is a functional parameter
+	Function struct {
+		Name   string   ` @Ident `
+		Params []*Param ` "(" (@@ {"," @@})? ")"`
+	}
+)
+
+var (
+	sqlLexer = lexer.MustSimple([]lexer.SimpleRule{
+		{`Keyword`, `(?i)\b(AND|OR|NOT|IN|LIKE)\b`},
+		{`Ident`, `[a-zA-Z_][a-zA-Z0-9_]*`},
+		{`Number`, `[-+]?\d*\.?\d+([eE][-+]?\d+)?`},
+		{`String`, `'[^']*'|"[^"]*"`},
+		{`Operators`, `!=|<=|>=|[,()=<>\]\[]`},
+		{"whitespace", `\s+`},
+	})
+
+	parser = participle.MustBuild[Expression](
+		participle.Lexer(sqlLexer),
+		participle.Unquote("String"),
+		participle.CaseInsensitive("Keyword"),
+	)
+)
+
+const (
+	StringParamID = "__string__"
+	NumberParamID = "__number__"
+	ArrayParamID  = "__array__"
+
+	// PfLValue the parameter can be a lvalue in the condition
+	PfLValue = 1 << 0
+	// PfRValue the parameter can be a rvalue in the condition
+	PfRValue = 1 << 1
+	// PfNop the parameter cannot have any operation
+	PfNop = 1 << 2
+	// PfComparable the parameter can be compared: <, >, !=, =, >=, <=
+	PfComparable = 1 << 3
+	// PfInLike the IN or LIKE operations are allowed for the param
+	PfInLike = 1 << 4
+)
+
+// id returns the param id by its type:
+// - string: StringParamID
+// - number: NumberParamID
+// - function: the function name
+// - identifier: the identifier name
+// - array: ArrayParamID
+func (p Param) id() string {
+	if p.Const != nil {
+		if p.Const.String != "" {
+			return StringParamID
+		}
+		return NumberParamID
+	}
+	if p.Function != nil {
+		return p.Function.Name
+	}
+	if p.Identifier != "" {
+		return p.Identifier
+	}
+	return ArrayParamID
+}
+
+// Name returns "value" of the constants (strings, numbers and the arrays) and names for the functions and identifiers
+func (p Param) Name(full bool) string {
+	if p.Const != nil {
+		return p.Const.Value()
+	}
+	if p.Function != nil {
+		return p.Function.Name
+	}
+	if p.Identifier != "" {
+		return p.Identifier
+	}
+
+	var sb strings.Builder
+	sb.WriteString("[")
+	for i, c := range p.Array {
+		if i > 0 {
+			sb.WriteString(", ")
+		}
+		if !full && i > 3 && len(p.Array) > 10 {
+			sb.WriteString(fmt.Sprintf("... and %d more", len(p.Array)-4))
+			break
+		}
+		sb.WriteString(c.Value())
+	}
+	sb.WriteString("]")
+	return sb.String()
+}
+
+// Value returns string value of the constant
+func (c Const) Value() string {
+	if c.String != "" {
+		return fmt.Sprintf("%q", c.String)
+	}
+	return fmt.Sprintf("%f", c.Number)
+}
+
+// Parse parses the expr and in case of success returns AST
+func Parse(expr string) (*Expression, error) {
+	expr = strings.TrimSpace(expr)
+	if len(expr) == 0 {
+		return &Expression{}, nil
+	}
+	e, err := parser.ParseString("", expr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse expression=%q: %w", expr, err)
+	}
+	return e, nil
+}
--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -10,19 +10,19 @@ type (
 	Logs interface {
 		// CreateLog creates a new log and returns its descriptor with the new ID
 		CreateLog(ctx context.Context, log *solaris.Log) (*solaris.Log, error)
-		// GetLogByID returns Log by its ID. It returns the errors.ErrNotExist if the log is marked for delete or
-		// or it doesn't exist
+		// GetLogByID returns Log by its ID. It returns the errors.ErrNotExist if the log
+		// is marked for delete, or it doesn't exist
 		GetLogByID(ctx context.Context, id string) (*solaris.Log, error)
 		// UpdateLog update the Log object information. The Log is matched by the log ID
 		UpdateLog(ctx context.Context, log *solaris.Log) (*solaris.Log, error)
 		// QueryLogs returns the list of Log objects matched to the query request
-		QueryLogs(ctx context.Context, qr QueryLogsReqeust) (*solaris.QueryLogsResult, error)
+		QueryLogs(ctx context.Context, qr QueryLogsRequest) (*solaris.QueryLogsResult, error)
 		// DeleteLogs allows to either mark or delete logs permanently
 		DeleteLogs(ctx context.Context, request DeleteLogsRequest) (*solaris.DeleteLogsResult, error)
 	}
 
-	// QueryLogsReqeust is used for selecting list of known logs
-	QueryLogsReqeust struct {
+	// QueryLogsRequest is used for selecting list of known logs
+	QueryLogsRequest struct {
 		Condition string
 		// IDs is the list of Log IDs should be selected. If the value is not empty, the Condition field is disregarded
 		IDs []string
@@ -35,7 +35,7 @@ type (
 	// DeleteLogsRequest specifies the DeleteLogs parameters
 	DeleteLogsRequest struct {
 		Condition string
-		// IDs is the list of Log IDs should be selected. If the value is not empty, the Condition field is disregarded
+		// IDs is the list of Log IDs should be deleted.
 		IDs []string
 		// MarkOnly allows not to delete the records physically, but mark it for deletion
 		MarkOnly bool
